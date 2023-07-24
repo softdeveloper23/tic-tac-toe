68ce95033e9b9aaac93f5cc3917b48af65f5ada5
unknown <brannongarrett77@gmail.com>
2023-07-24 16:53:00 +0200

    Added an additional button and div tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <div>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+  );
 }
