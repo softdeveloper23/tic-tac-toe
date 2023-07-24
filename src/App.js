e7c4d942c448b523d423f5dc857bcc3e1d7a7537
unknown <brannongarrett77@gmail.com>
2023-07-24 17:33:06 +0200

    Added a destructured prop value to Square

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
