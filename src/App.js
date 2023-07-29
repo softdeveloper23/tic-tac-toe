a79b395fef45cd2dfc61e8e5c076f289273f4f6f
unknown <brannongarrett77@gmail.com>
2023-07-29 14:40:48 +0200

    Added a new save

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
   return (
-    <
-      button className="square"
-      onClick={onSquareClick}
-    >
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
