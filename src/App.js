3507e1a07eb491bd0386bc6937814a59b530dedf
unknown <brannongarrett77@gmail.com>
2023-07-29 17:29:43 +0200

    Fixed error and cleaned up code

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  setXIsNext(nextMove % 2 === 0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
